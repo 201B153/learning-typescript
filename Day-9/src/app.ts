59abae9b65a94547a965a976268e32f7b420bbfe
201b153 <tomarm698@gmail.com>
2022-09-20 01:56:41 +0530

    Day-9 II

--- Day-9/src/app.ts
+++ Day-9/src/app.ts
@@ -54,16 +54,100 @@ console.log(mergedObj1.age);
 
 // lentgh is not sure thing for ts so
 interface Length {
-    length: number;
+  length: number;
 }
 function countAndprint<T extends Length>(element: T): [T, string] {
-    let descriptionText = 'No value'
-    if (element.length === 1) {
-        descriptionText = 'Got 1 elemnt  ';
-    }
-    if (element.length > 1) {
-        descriptionText = 'Got  ' + element.length + ' elements';
-    }
-    return [element, descriptionText];
+  // To be more specific we can set return type to be array or anything else
+  // first element will be off of genric type while other will be off array or ouput type
+  let descriptionText = 'No value';
+  if (element.length === 1) {
+    descriptionText = 'Got 1 elemnt  ';
+  }
+  if (element.length > 1) {
+    descriptionText = 'Got  ' + element.length + ' elements';
+  }
+  return [element, descriptionText];
 }
 console.log(countAndprint('hi, there generic'));
+
+// the keyoff contraint
+function extractAndconvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  //the key of contraint which sates the key of other genric type
+  // Also that the key is the property of first type
+  return 'Value' + obj[key];
+}
+
+console.log(extractAndconvert({ name: 'Max' }, 'name'));
+
+// Generic classes
+class DataStorage<T> {
+  private data: T[] = [];
+
+  additem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    if (this.data.indexOf(item) === -1) {
+      return;
+    }
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItem() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.additem('mayank');
+textStorage.additem('tomar');
+textStorage.removeItem('tomar');
+console.log(textStorage.getItem());
+
+const numberStorage = new DataStorage<number | boolean>();
+// just like this we ca give genric class any type we want and canchange later
+// Use class for primitive type in genric class
+// bcz of that issue objects are rarely used in generic class as type
+const objStorage = new DataStorage<object>();
+objStorage.additem({ name: 'mayank' });
+objStorage.additem({ name: 'tomar' });
+objStorage.removeItem({ name: 'tomar' });
+console.log(objStorage.getItem());
+// we cannot remove refercence like objs
+
+// Generic type give us flexibility combine with type safety
+// Array and promise are built in genric type
+
+// BUILT-IN types
+// PARTIAL type
+interface Course {
+  title: string;
+  description: string;
+  completeUntil: Date;
+}
+
+function createCourse(title: string, description: string, date: Date): Course {
+  // return { title: title, description: description, completeUtil: date};
+  let course: Partial<Course> = {};
+  // Partial type make s all the values in type an optional value 
+  course.title = title;
+  course.description = description;
+  course.completeUntil = date;
+  return course as Course;
+}
+
+// READONLY type
+// marks the property method or anything not editable 
+const names4: Readonly<string[]> = ['mayank', 'hello'];
+// Cannot acess them
+// names4.push('toamr')
+// names4.pop('toamr')
+
+// Union type have limitation on type changing while generics allow auto change of type
+// union allows changing type in mid while genric doesn't
+// generics locks a type for whole class or function while union type is flexible
+

3bd0b055c53b808810c211d9fe7150683e1633a4
201b153 <tomarm698@gmail.com>
2022-09-25 03:17:02 +0530

    Day-14 I

---
+++ Day-14/src/App.tsx
@@ -0,0 +1,12 @@
+// doing react in type way
+// React.FC means that the App should be function type in react
+const App: React.FC = () => {
+  // FC can also be replaced by FunctionaComponent
+  return (
+    <div className="App">
+      <h1>Hello ts and react</h1>
+    </div>
+  );
+};
+
+export default App;
